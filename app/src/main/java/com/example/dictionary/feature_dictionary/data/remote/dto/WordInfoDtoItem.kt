9eef56ddc3d2afa47d1b6dd6fcd1e2e525e9f69d
Ahmed Nasr <58734390+ahmeednasr@users.noreply.github.com>
2024-05-27 23:58:45 +0300

    init dto and mapper

---
+++ app/src/main/java/com/example/dictionary/feature_dictionary/data/remote/dto/WordInfoDtoItem.kt
@@ -0,0 +1,30 @@
+package com.example.dictionary.feature_dictionary.data.remote.dto
+
+
+import com.example.dictionary.feature_dictionary.domain.model.WordInfo
+import com.google.gson.annotations.SerializedName
+
+data class WordInfoDtoItem(
+    @SerializedName("license")
+    val license: License?,
+    @SerializedName("meanings")
+    val meanings: List<MeaningDto>?,
+    @SerializedName("phonetic")
+    val phonetic: String?,
+    @SerializedName("phonetics")
+    val phonetics: List<PhoneticDto>?,
+    @SerializedName("sourceUrls")
+    val sourceUrls: List<String>?,
+    @SerializedName("word")
+    val word: String?
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            license = license,
+            meanings = meanings?.map { it.toMeaning() },
+            phonetic = phonetic,
+            sourceUrls = sourceUrls,
+            word = word
+        )
+    }
+}
